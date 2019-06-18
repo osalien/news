8466a099791b3bdb768f3f610538e85403e86230
yuanzhao <1578851175@qq.com>
2019-06-18 15:29:50 +0800

    每日分享

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,6 @@ git init
 git add -A
 git commit -m '每日分享'
 
-<<<<<<< HEAD
 git push -u git@github.com:osalien/docker.git master
-=======
-git push -f git@github.com:osalien/docker.git master
->>>>>>> ff61b433fea1f26a6151680bbaeb37ce3dcbea8c
 
 cd -
